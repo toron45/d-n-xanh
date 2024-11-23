bf792962d03f9292a8731c0eed3ff9436b4517c3
toron45 <nguyenvanhung.vn40@gmail.com>
2024-11-23 15:07:42 +0700

    upload source code

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,9 @@
 console.log('JavaScript đã được liên kết thành công!');
+document.addEventListener("DOMContentLoaded", () => {
+    ScrollReveal().reveal('.project-card', {
+        duration: 1000,
+        distance: '50px',
+        origin: 'bottom',
+        reset: true,
+    });
+});
